6d5d0b999090530c9fb41df5198c56de49dd0cbc
Ludovico Loreti <n4pst3r@N4pst3r-Work.local>
2022-03-23 12:15:40 +0100

    making this thing actually usable.

---
+++ pyncat.py
@@ -0,0 +1,135 @@
+# -----------------------------------------------------------
+# Netcat substitute written in Python 3
+#
+# (C) 2022 Ludovico Loreti
+# Released under MIT License
+# 
+# twitter.com/ludovico.loreti
+# -----------------------------------------------------------
+
+import argparse
+import socket
+import shlex
+import subprocess
+import sys
+import textwrap
+import threading
+
+                     
+def exec(cmd):
+    cmd = cmd.strip()
+    if not cmd:
+        return
+    output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
+    return output.decode()
+
+
+class NetCat:
+    def __init__(self, args, buffer=None):
+        self.args = args
+        self.buffer = buffer
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        
+    def run(self):
+        if self.args.listen:
+            self.listen()
+        else:
+            self.send()
+    
+    def send(self):
+        self.socket.connect((self.args.target, self.args.port))
+        if self.buffer:
+            self.socket.send(self.buffer)
+        
+        try:
+            while True:
+                recv_len = 1
+                response = ''
+                while recv_len:
+                    data = self.socket.recv(4096)
+                    recv_len = len(data)
+                    response += data.decode()
+                    if recv_len < 4096:
+                        break
+                if response:
+                    print(response)
+                    buffer = input('nc: #> ')
+                    buffer += '\n'
+                    self.socket.send(buffer.encode())
+        except KeyboardInterrupt:
+            print('User terminated.')
+            self.socket.close()
+            sys.exit()
+            
+    def listen(self):
+        print('listening')
+        self.socket.bind((self.args.target, self.args.port))
+        self.socket.listen(5)
+        while True:
+            client_socket, _ = self.socket.accept()
+            client_thread = threading.Thread(target=self.handle, args=(client_socket,))
+            client_thread.start()
+            
+    def handle(self, client_socket):
+        if self.args.exec:
+            output = exec(self.args.exec)
+            client_socket.send(output.encode())
+            
+        elif self.args.upload:
+            file_buffer = b''
+            while True:
+                data = client_socket.recv(4096)
+                if data:
+                    file_buffer += data
+                    print(len(file_buffer))
+                else:
+                    break
+                
+            with open(self.args.upload, 'wb') as f:
+                f.write(file_buffer)
+            message = f'Saved file {self.args.upload}'
+            client_socket.send(message.encode())
+            
+        elif self.args.cmd:
+            cmd_buffer = b''
+            while True:
+                try:
+                    client_socket.send(b' <Command> ')
+                    while '\n' not in cmd_buffer.decode():
+                        cmd_buffer += client_socket.recv(64)
+                    response = exec(cmd_buffer.decode())
+                    if response:
+                        client_socket.send(response.encode())
+                    cmd_buffer = b''
+                except Exception as e:
+                    print(f'Server killed {e}')
+                    self.socket.close()
+                    sys.exit()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='pyNetcat',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=textwrap.dedent('''Usage:
+            pyncat.py -t IP -p PORT -l -c # command shell
+            pyncat.py -t IP -p PORT -l -u=file.txt # upload to file
+            pyncat.py -t IP -p PORT -l -e=\"cat /etc/passwd\" # exec one shell cmd
+            echo 'ABCDEFGHI' | ./pyncat.py -t IP -p PORT # echo local text to server port $PORT
+            pyncat.py -t IP -p PORT # connect to server
+            '''))
+    parser.add_argument('-c', '--cmd', action='store_true', help='Initialize command shell')
+    parser.add_argument('-e', '--exec', help='Execute specified command')
+    parser.add_argument('-l', '--listen', action='store_true', help='Listen')
+    parser.add_argument('-p', '--port', type=int, default=5555, help='Specified PORT')
+    parser.add_argument('-t', '--target', default='192.168.0.156', help='Specified IP')
+    parser.add_argument('-u', '--upload', help='Uplaod file')
+    args = parser.parse_args()
+    if args.listen:
+        buffer = ''
+    else:
+        buffer = sys.stdin.read()
+    
+    nc = NetCat(args, buffer.encode('utf-8'))
+    nc.run()
